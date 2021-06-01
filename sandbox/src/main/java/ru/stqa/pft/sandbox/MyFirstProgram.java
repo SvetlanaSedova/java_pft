48f0591149f9c8f6f0daa51ecc8c9c6bf271e8f2
SvetlanaSedova <Myapril1991@gmail.com>
2021-06-01 19:26:28 +0300

    Добавлены классы Square и Rectangle

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,29 +2,25 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-    public static void main(String[] args) {
-       hello("world");
-       hello("user");
-       hello("sdfsdfd");
-       double l =5;
-        System.out.println("Площадь квадрата со стороной "+ l + " = " + area(l));
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("sdfsdfd");
 
-        double a=4;
-        double b=6;
+    Square s = new Square(5);
 
-        System.out.println("Площадь прямоуголника со сторонами " + a + " и "+ b +" = " + area(a,b));
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    }
+    Rectangle r = new Rectangle(4, 6);
 
-    public static void hello(String somebody){
-           System.out.println("Hello, "+ somebody+ "!");
-    }
+    System.out.println("Площадь прямоуголника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-    public static double area(double len){
-        return len*len;
-    }
+  }
 
-    public static double area(double a,double b){
-        return a * b;
-    }
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+ 
+
 }
