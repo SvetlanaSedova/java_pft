0cdf8f34f4c62703a07b5d7a04790d0a75d971e3
SvetlanaSedova <Myapril1991@gmail.com>
2021-06-06 16:16:17 +0300

    задание 4
    создан тест по созданию контакта testContactCreation
    выделены вспомогательные методы (goToAddingNewContact, fillAddingContactForm, submitAddingContact, returnHomePage)
    параметризован метод заполнения формы создания контакта fillAddingContactForm с использованием вспомогательного класса ContactData

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,55 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstName;
+  private final String middleName;
+  private final String lastName;
+  private final String nick;
+  private final String address;
+  private final String phoneHome;
+  private final String phoneMobile;
+  private final String email;
+
+  public ContactData(String firstName, String middleName, String lastName, String nick, String address, String phoneHome, String phoneMobile, String email) {
+    this.firstName = firstName;
+    this.middleName = middleName;
+    this.lastName = lastName;
+    this.nick = nick;
+    this.address = address;
+    this.phoneHome = phoneHome;
+    this.phoneMobile = phoneMobile;
+    this.email = email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getNick() {
+    return nick;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getPhoneHome() {
+    return phoneHome;
+  }
+
+  public String getPhoneMobile() {
+    return phoneMobile;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
