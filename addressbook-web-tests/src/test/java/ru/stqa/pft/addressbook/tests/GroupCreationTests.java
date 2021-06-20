50ae75c9261642a3e2fbc8e38896fd9e916cc2d2
SvetlanaSedova <Myapril1991@gmail.com>
2021-06-20 15:58:50 +0300

    Доработка для групп
    сравнение списков в тестах на удаление/изменение и создание группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,15 +4,42 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigatioHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before+1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+//    int before = app.getGroupHelper().getGroupCount();
+    GroupData group = new GroupData("test2", "test2", "test3");
+    app.getGroupHelper().createGroup(group);
+//    int after = app.getGroupHelper().getGroupCount();
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() + 1);
+
+
+//    int max = 0;
+//    for (GroupData g : after) {
+////      max = g.getId() > max ? g.getId() : max;
+//      if(g.getId() > max){
+//        max = g.getId();
+//      }
+//    }
+  //  Comparator<? super GroupData> byId= (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+
+//    int max1 = after.stream().max((Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
+
+    group.setId(after.stream().max((Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    before.add(group);
+    Comparator<? super GroupData> byId=(g1,g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
+//    Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
   }
 
 }
