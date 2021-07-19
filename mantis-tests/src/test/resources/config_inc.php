778e0c9e56e71f8b248baf49f8c662c24b48d293
SvetlanaSedova <Myapril1991@gmail.com>
2021-07-19 15:00:32 +0300

    FTP
    Отключение капчи

---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,8 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+$g_crypto_master_salt     = 'aRO7OY1pgA2M76dC05H8YSeJq6byizoi+Bb2MM5EWoo=';
+$g_signup_use_captcha     = OFF;
